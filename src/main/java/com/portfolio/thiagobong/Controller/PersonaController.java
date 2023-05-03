e6e293b7c9a7c639b1f4cb6328cfd8b0cafa6490
thiagobong22 <117801113+thiagobong22@users.noreply.github.com>
2023-05-02 23:17:51 -0300

    Creación del BackEnd
    Se creo el BackEnd

---
+++ src/main/java/com/portfolio/thiagobong/Controller/PersonaController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.thiagobong.Controller;
+
+import com.portfolio.thiagobong.Entity.Persona;
+import com.portfolio.thiagobong.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    @Autowired IPersonaService ipersonaService;
+    
+    @GetMapping("Personas/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+    
+    @PostMapping("/persona/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "La persona fue creada correctamente";
+    }
+    
+    @DeleteMapping("/persona/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "La persona fue eliminada correctamente";
+    }
+    
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+            @RequestParam("nombre") String nuevoNombre,
+            @RequestParam("apellido") String nuevoApellido,
+            @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
+}
---
+++ src/main/java/com/portfolio/thiagobong/Interface/IPersonaService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.portfolio.thiagobong.Interface;
+
+import com.portfolio.thiagobong.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+   //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objetos pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
